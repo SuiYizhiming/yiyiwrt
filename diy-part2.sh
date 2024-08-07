e30d198770f0533d57cbb6d47e36d304402bd68c
SuiYizhiming <88370888+SuiYizhiming@users.noreply.github.com>
2024-08-07 09:43:23 +0800

    Update diy-part2.sh
    个性化选项24.8.7

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,16 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#适配小米路由器的默认地址
+sed -i 's/192.168.1.1/192.168.31.100/g' package/base-files/files/bin/config_generate
+
+# 加入编译者信息
+sed -i "s/OpenWrt /yiyi build $(TZ=UTC-8 date "+%Y.%m.%d") @ YiyiWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#名称替换openwrt->yiwrt
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='YiWrt'' package/lean/default-settings/files/zzz-default-settings
