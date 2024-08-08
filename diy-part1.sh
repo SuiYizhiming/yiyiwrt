0fcb72d0c9ca8f2e19b55ca82d4de4cdd0193fbb
SuiYizhiming <88370888+SuiYizhiming@users.noreply.github.com>
2024-08-08 12:33:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,15 +14,15 @@
 #编译教程参考https://fairysen.com/819.html
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source 24.8.7
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'  >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
-#echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
  
